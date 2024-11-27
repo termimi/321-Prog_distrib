225c34f8cd6c8c204af94dbc728c69e1ef81430e
termimi <123395118+termimi@users.noreply.github.com>
2024-11-27 10:14:23 +0100

    exo oke

--- activites/transat/transat-master/transat-master/Transat/NonResilientPublisher.cs
+++ activites/transat/transat-master/transat-master/Transat/NonResilientPublisher.cs
@@ -2,11 +2,14 @@ namespace Transat;
 
 public class NonResilientPublisher(FaillibleQos0Storage storage1, FaillibleQos0Storage storage2)
 {
-    private readonly string _id = Guid.NewGuid().ToString("n");
-    
+    private readonly string _id = Guid.NewGuid().ToString(format: "n");
     public void Send(int message)
     {
-        storage1.Store(_id,message);
-        storage2.Store(_id, message);
+        var uniqueId = $"{_id}-{Guid.NewGuid().ToString(format: "n")}";
+        while (!storage1.Data.Keys.Any(key => key.StartsWith(uniqueId)))
+            storage1.Store(uniqueId, message);
+
+        while (!storage2.Data.Keys.Any(key => key.StartsWith(uniqueId)))
+            storage2.Store(uniqueId, message);
     }
 }
